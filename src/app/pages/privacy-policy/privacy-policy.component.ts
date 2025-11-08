a5a3066284c25cb254a686b0a92e1554106e0142
Adam ONeill <adamaoneill@gmail.com>
2025-11-08 12:37:48 +0000

    privacy policy page

--- src/app/pages/privacy-policy/privacy-policy.component.ts
+++ src/app/pages/privacy-policy/privacy-policy.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../../common/components/header/header.component';
-import { FooterComponent } from '../../common/components/footer/footer.component';
 import { PageTemplateComponent } from '../../common/components/page-template/page-template.component';
 import { WhitePageBodyComponent } from '../../common/components/white-page-body/white-page-body.component';
+import { ExpandableCardComponent } from '../../common/components/expandable-card/expandable-card.component';
 
 @Component({
   selector: 'app-privacy-policy',
-  imports: [PageTemplateComponent, WhitePageBodyComponent],
+  imports: [PageTemplateComponent, ExpandableCardComponent],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss',
 })
